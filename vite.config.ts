027f2f8178d025bcc7cb5867954eb4249a03283a
vedikabhoyar61-debug <vedikabhoyar61@gmail.com>
2026-09-06 21:46:38 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/chatbot/',
-  plugins: [react()],
-})
+  base: "/ai-government-scheme-chatbot/",
+});
